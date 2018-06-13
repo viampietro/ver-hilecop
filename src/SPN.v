045d27507c7c797ab094f8df829f8e517fc6d0f7
Mathieu Lasjaunias <mathieu.lasjaunias@lirmm.fr>
2018-06-13 15:09:04 +0200

    hint for specification of SPN

--- src/SPN.v
+++ src/SPN.v
@@ -970,7 +970,16 @@ Compute
 
 
 
+Inductive step : SPN -> SPN -> Prop := 
+| trans_sync: forall (x y : SPN),
+    (y = snd (spn_fired x))  ->  (step x y)
 
+.  
+
+
+                             
+Definition is_the_algorithm (f : SPN -> SPN) :=
+  forall spn, (* Permutation al (f al) /\ sorted (f al) *) . 
 
 
 
